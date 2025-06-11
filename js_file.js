0e5aed290ebf282ccdb10779897f0f4b929c76d2
Kalyani Pebbuli <116427058+206W1A0439@users.noreply.github.com>
2025-06-11 23:05:06 +0530

    Add files via upload

---
+++ js_file.js
@@ -0,0 +1,54 @@
+document.body.style.backgroundColor="lightblue"
+document.body.style.textAlign="center"
+document.body.style.padding="50px"
+
+
+let heading=document.createElement("h1")
+heading.innerHTML=`<b><u>BIO DATA</u></b>`
+heading.style.color="orange"
+document.body.append(heading)
+
+let name_element=document.createElement("h2")
+name_element.innerHTML=`<b>Name: </b><i>Kalyani</i>`
+name_element.style.color="blue"
+document.body.append(name_element)
+
+let date_of_birth=document.createElement("h2")
+date_of_birth.innerHTML=`<b>DOB: </b><i>16-01-2003</i>`
+date_of_birth.style.color="blue"
+document.body.append(date_of_birth)
+
+let qualification_element=document.createElement("h2")
+qualification_element.innerHTML=`<b>Qualification: </b><i>B.Tech</i>`
+qualification_element.style.color="blue"
+document.body.append(qualification_element)
+
+let address_element=document.createElement("h2")
+address_element.innerHTML=`<b>ADDRESS: </b><i>1-1,Mangavaram Village,Vizag</i>`
+address_element.style.color="blue"
+document.body.append(address_element)
+
+let Skills_element=document.createElement("h2")
+Skills_element.innerHTML=`<b>Skills: </b><i>Python, SQL, HTML, CSS, JS</i>`
+Skills_element.style.color="blue"
+document.body.append(Skills_element)
+
+let hobbies_element=document.createElement("h2")
+hobbies_element.innerHTML=`<b>Hobbies:  </b><i>Listening Songs, Solve Sudokos, Play with rubix cube</i>`
+hobbies_element.style.color="blue"
+document.body.append(hobbies_element)
+
+let email_element=document.createElement("h2")
+email_element.innerHTML=`<b>Email: </b><i>kalyani414425@gmail.com</i>`
+email_element.style.color="blue"
+document.body.append(email_element)
+
+let phone_element=document.createElement("h2")
+phone_element.innerHTML=`<b>Phone_No: </b><i>9392538992</i>`
+phone_element.style.color="blue"
+document.body.append(phone_element)
+
+let nationality_element=document.createElement("h2")
+nationality_element.innerHTML=`<b>Nationality: </b><i>INDIAN</i>`
+nationality_element.style.color="blue"
+document.body.append(nationality_element)
